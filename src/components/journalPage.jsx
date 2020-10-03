38d52aad4bc0c8973f5d213412c132c21451fa67
Evan <evanmorrow@gmail.com>
2020-10-03 16:23:27 -0700

    work in progress on the word bubble component for the journal page.

--- src/components/journalPage.jsx
+++ src/components/journalPage.jsx
@@ -1,12 +1,39 @@
-import JournalTable from "./journalTable";
-
 import React from "react";
+import JournalTable from "./journalTable";
+import ReactWordcloud from "react-wordcloud";
+
+const words = [
+  {
+    text: "told",
+    value: 64,
+  },
+  {
+    text: "mistake",
+    value: 11,
+  },
+  {
+    text: "thought",
+    value: 16,
+  },
+  {
+    text: "bad",
+    value: 17,
+  },
+];
 
 function JournalPage(props) {
   return (
-    <div>
-      <JournalTable />
-    </div>
+    <>
+      <div className="row">
+        <div className="col-6">
+          <ReactWordcloud words={words} />
+        </div>
+        <div className="col-6">BOOBS</div>
+      </div>
+      <div>
+        <JournalTable />
+      </div>
+    </>
   );
 }
 
