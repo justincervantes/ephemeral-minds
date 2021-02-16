98a7aee407227c46cae5123c763b2d4bf12b3a9b
Justin Cervantes <cervantes.jfa@gmail.com>
2021-02-15 17:40:35 -0700

    Adding anlaytics

--- src/components/requestFeature.jsx
+++ src/components/requestFeature.jsx
@@ -31,7 +31,7 @@ function RequestFeature() {
                     <label htmlFor="exampleFormControlTextarea1">Description of feature request</label>
                     <textarea onChange={handleChange} className="form-control" rows="3"></textarea>
                 </div>
-                <p><small>Note: Feature requests are sent to all active developers and are integrated into our roadmap whereever possible. If the development team has questions about your request, they will contact you at the email associated with your account. If you have any questions or concerns please contact the project lead, Justin at cervantes.jfa@gmail.com</small></p>
+                <p><small>Note: Feature requests are things you'd like to be able to do on this website. Requests are sent to all active developers and are integrated into our roadmap wherever possible. If the development team has questions about your request or are unable to implement the request, they will contact you at the email associated with your account. If you have any questions or concerns please contact the project lead, Justin at cervantes.jfa@gmail.com</small></p>
                 <button disabled={feature ? false : true} type="submit" className="btn btn-primary">Submit</button>
             </form>
         </>
